179063938ed5db03094a44a2e2f6cb4241b7f9e3
Leonardo-Figueiredo-do-Nascimento <kriptonascimento@hotmail.com>
2022-10-26 13:05:45 -0300

    Adição do contador de flexões
    implementação de um contador de flexões em vídeos
    usando as bibliotecas opencv-python e mediapipe

---
+++ src/package/exercicios/flexoes.py
@@ -0,0 +1,52 @@
+import cv2
+import mediapipe as mp
+import math
+video = cv2.VideoCapture('push-ups.mp4')
+#variavel que traz os modelos do corpo humano
+pose = mp.solutions.pose
+#variavel responsavel pela detecção
+Pose = pose.Pose(min_tracking_confidence=0.5,min_detection_confidence=0.5)
+#variavel para desenhar os pontos e suas ligações na imagem
+draw = mp.solutions.drawing_utils
+#contador de flexões
+contador = 0
+# variavel para controlar a contagem de flexões
+check = True
+while True:
+    sucess, img = video.read()
+    #analisa o corpo da pessoa
+    results = Pose.process(img)
+    #pontos do corpo humano
+    points = results.pose_landmarks
+    #linhas no corpo
+    draw.draw_landmarks(img,points,pose.POSE_CONNECTIONS)
+    #variaveis da altura,largura e numero de canais da imagem
+    h, w, _ = img.shape
+
+    #variaveis para os pontos, com os pontos ajustados ao tamanho da tela
+    if points:
+        ombroDx = int(points.landmark[pose.PoseLandmark.RIGHT_SHOULDER].x * w)
+        ombroDy = int(points.landmark[pose.PoseLandmark.RIGHT_SHOULDER].y * h)
+        pulsoDx = int(points.landmark[pose.PoseLandmark.RIGHT_WRIST].x * w)
+        pulsoDy = int(points.landmark[pose.PoseLandmark.RIGHT_WRIST].y * h)
+
+        #distancia entre mão e o ombro
+        distMaoxombro = math.hypot(ombroDx - pulsoDx, ombroDy - pulsoDy)
+
+        # contagem da flexão
+        if check == True and distMaoxombro <= 70:
+            contador +=1
+            check = False
+
+        if distMaoxombro >=100 :
+            check = True
+
+        #Texto na tela
+        texto = f'Contagem = {contador}'
+        cv2.putText(img,texto,(40,50),cv2.FONT_HERSHEY_SIMPLEX,1,(255,200,255),3)
+
+    cv2.imshow('Resutado',img)
+    #tecla para encerrar o loop e fechar o programa
+    if cv2.waitKey(40) & 0xFF == ord('q'):
+        break
+
