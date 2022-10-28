f3f0cc517b28b5eb18f42a26eed2761603c3acaf
Giovani Giaqueto de Oliveira <giovanigiaqueto@gmail.com>
2022-10-28 13:52:44 -0300

    Encapsulamento do contador de flexões em função
    encapsulamento do contador de flexões dentro de uma função
    que aceita uma fonte de vídeo, renderizando ele em uma janela
    e contando as flexões. isso permite que o contador seja usado
    por outras partes do código sem executá-lo imediatamente
    
    opcionalmente, o contador pode ser executado como módulo pelo
    terminal, contando as flexões usando o arquivo videos/push-ups.mp4

--- src/package/exercicios/flexoes.py
+++ src/package/exercicios/flexoes.py
@@ -8,52 +8,66 @@ import math
 # biblioteca do programa
 from package.video import abrir_video, extrair_frames
 
-# variável que traz os modelos do corpo humano
-pose = mp.solutions.pose
-# variável responsável pela detecção
-Pose = pose.Pose(min_tracking_confidence=0.5,min_detection_confidence=0.5)
-# variável para desenhar os pontos e suas ligações na imagem
-draw = mp.solutions.drawing_utils
-# contador de flexões
-contador = 0
-# variável para controlar a contagem de flexões
-check = True
+def contar(video):
+    """
+    Aplica detecção de poses no vídeo passado como parâmetro,
+    contando a quantidade de flexões feitas.
 
-arquivo = os.path.join('videos', 'push-ups.mp4')
-with abrir_vide(arquivo) as captura:
-    for img in extrair_frames(captura):
-        results = Pose.process(img)
-        # pontos do corpo humano
-        points = results.pose_landmarks
-        # linhas no corpo
-        draw.draw_landmarks(img,points,pose.POSE_CONNECTIONS)
-        # variáveis da altura,largura e numero de canais da imagem
-        h, w, _ = img.shape
+    Como essa função utiliza a funções do módulo package.video,
+    ela aceita tanto nomes de arquivos de vídeo como índices
+    de dispositivos que o opencv-python ou pyopencv podem utilizar
+    """
 
-        # variáveis para os pontos, com os pontos ajustados ao tamanho da tela
-        if points:
-            ombroDx = int(points.landmark[pose.PoseLandmark.RIGHT_SHOULDER].x * w)
-            ombroDy = int(points.landmark[pose.PoseLandmark.RIGHT_SHOULDER].y * h)
-            pulsoDx = int(points.landmark[pose.PoseLandmark.RIGHT_WRIST].x * w)
-            pulsoDy = int(points.landmark[pose.PoseLandmark.RIGHT_WRIST].y * h)
+    # variável que traz os modelos do corpo humano
+    pose = mp.solutions.pose
+    # variável responsável pela detecção
+    Pose = pose.Pose(min_tracking_confidence=0.5,min_detection_confidence=0.5)
+    # variável para desenhar os pontos e suas ligações na imagem
+    draw = mp.solutions.drawing_utils
+    # contador de flexões
+    contador = 0
+    # variável para controlar a contagem de flexões
+    check = True
 
-            # distancia entre mão e o ombro
-            distMaoxombro = math.hypot(ombroDx - pulsoDx, ombroDy - pulsoDy)
+    with abrir_video(video) as captura:
+        for img in extrair_frames(captura):
 
-            # contagem da flexão
-            if check == True and distMaoxombro <= 70:
-                contador +=1
-                check = False
+            results = Pose.process(img)
+            # pontos do corpo humano
+            points = results.pose_landmarks
+            # linhas no corpo
+            draw.draw_landmarks(img,points,pose.POSE_CONNECTIONS)
+            # variáveis da altura,largura e numero de canais da imagem
+            h, w, _ = img.shape
 
-            if distMaoxombro >=100 :
-                check = True
+            # variáveis para os pontos, com os pontos ajustados ao tamanho da tela
+            if points:
+                ombroDx = int(points.landmark[pose.PoseLandmark.RIGHT_SHOULDER].x * w)
+                ombroDy = int(points.landmark[pose.PoseLandmark.RIGHT_SHOULDER].y * h)
+                pulsoDx = int(points.landmark[pose.PoseLandmark.RIGHT_WRIST].x * w)
+                pulsoDy = int(points.landmark[pose.PoseLandmark.RIGHT_WRIST].y * h)
 
-            # texto na tela
-            texto = f'Contagem = {contador}'
-            cv2.putText(img,texto,(40,50),cv2.FONT_HERSHEY_SIMPLEX,1,(255,200,255),3)
+                # distancia entre mão e o ombro
+                distMaoxombro = math.hypot(ombroDx - pulsoDx, ombroDy - pulsoDy)
 
-        cv2.imshow('Resutado',img)
-        # tecla para encerrar o loop e fechar o programa
-        if cv2.waitKey(40) & 0xFF == ord('q'):
-            break
+                # contagem da flexão
+                if check == True and distMaoxombro <= 70:
+                    contador +=1
+                    check = False
+
+                if distMaoxombro >=100 :
+                    check = True
+
+                # texto na tela
+                texto = f'Contagem = {contador}'
+                cv2.putText(img,texto,(40,50),cv2.FONT_HERSHEY_SIMPLEX,1,(255,200,255),3)
+
+            cv2.imshow('Resutado',img)
+            # tecla para encerrar o loop e fechar o programa
+            if cv2.waitKey(40) & 0xFF == ord('q'):
+                break
+
+if __name__ == "__main__":
+    arquivo = os.path.join("videos", "push-ups.mp4")
+    contar(arquivo)
 
