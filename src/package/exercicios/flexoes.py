2656e6b535be10109d6f6989c1cd57e8320d833a
Giovani Giaqueto de Oliveira <giovanigiaqueto@gmail.com>
2022-10-30 16:48:10 -0300

    Modificação na execução do módulo package.exercicios.flexoes
    modificação do módulo package.exercicios.flexoes para que
    o arquivo ou dispositivo de vídeo a ser usado para a contagem
    seja fornecido pela linha de comando ao invés de estar no
    código fonte

--- src/package/exercicios/flexoes.py
+++ src/package/exercicios/flexoes.py
@@ -4,6 +4,7 @@ import cv2
 import mediapipe as mp
 # biblioteca padrão
 import os
+import sys
 import math
 # biblioteca do programa
 from package.video import abrir_video, extrair_frames
@@ -68,6 +69,28 @@ def contar(video):
                 break
 
 if __name__ == "__main__":
-    arquivo = os.path.join("videos", "push-ups.mp4")
+    # mostra a ajuda
+    if len(sys.argv) != 2:
+        print(
+            f"usagem: {sys.argv[0]} -m package.exercicios.flexao (arquivo ou índice de dispositivo)\n\n"
+             "abre o arquivo de vídeo ou dispositivo indicado\n"
+             "por seu índice e conta as flexões"
+        )
+        exit(1)
+
+    # processa o argumento de entrada (arquivo ou índice de dispositivo de vídeo)
+    try:
+        arquivo = int(sys.argv[1])
+    except ValueError:
+        arquivo = os.path.abspath(sys.argv[1])
+
+    # checa se o arquivo está correto
+    if isinstance(arquivo, str):
+        if not os.path.exists(arquivo):
+            print(f"erro: '{arquivo}' não pode ser encontrado", file=sys.stderr)
+        if not os.path.isfile(arquivo):
+            print(f"erro: '{arquivo}' não é um arquivo", file=sys.stderr)
+
+    # contagem das flexões
     contar(arquivo)
 
