7d949dd64fa4cceebf5e6ac71aa5f446e6750700
Giovani Giaqueto de Oliveira <giovanigiaqueto@gmail.com>
2022-10-31 14:21:26 -0300

    Correção de import circular
    reordenação dos imports dos exercícios e das definições
    das funções de registro, listagem e contagem de exercícios
    pelo nome para evitar import circular (a função de registro
    de exercícios deve ser importada antes dos exercícios)
    
    transferência das funções de registro, listagem e contagem de
    exercícios pelo nome para um novo módulo interno

--- src/package/exercicios/__init__.py
+++ src/package/exercicios/__init__.py
@@ -1,70 +1,21 @@
 
+# importa as funções de registro e listagem de exercícios
+from package.exercicios import _registro
+from package.exercicios._registro import *
+
+# funções de registro acessíveis via "from module import *"
+__all__ = [*_registro.__all__]
+
+# import dos exercícios
+# NOTE: não mova os imports pra antes dos imports
+#       das funções de registro de exercícios,
+#       isso causará um erro de import circular
 from package.exercicios import flexoes
 
-__all__ = ["flexoes"]
+# módulos dos exercícios acessíveis via "from module import *"
+__all__.extend(["flexoes"])
 
-__exercicios = {}
-
-def registrar_exercicio(nome_exercicio):
-    """
-    registra a função seguinte como um contador de exercício
-    usando um decorador, o parâmetro deve ser o nome do exercício,
-    e a função retornada deve ser chamada no contador de exercício
-
-    exemplo:
-
-    @registrar_exercicio("polichinelo")
-    def contar_polichinelos(video):
-        ... implementação ...
-
-    """
-
-    if nome_exercicio in __exercicios:
-        raise RuntimeError(f"um exercício com o nome '{nome_exercicio}' já foi registrado")
-
-    # cria e retorna o decorador
-    def decorator(funcao):
-        global __exercicios
-        __exercicios[nome_exercicio] = funcao
-        return funcao
-
-    return decorador
-
-def listar_exercicios():
-    """
-    retorna uma lista de exercícios conhecidos
-    """
-    return list(__exercicios.keys())
-
-def contar_exercio(exercicio, video):
-    # checagem de tipos
-    if not isinstance(exercicio, str):
-        raise TypeError("esperado uma str para o parâmetro 'exercicio', "
-                        f"recebido um valor do tipo {type(exercicio).__qualname__}")
-    if not isinstance(video, (str, int)):
-        raise TypeError("esperado uma str ou int para o parâmetro 'video', "
-                        f"recebido um valor do tipo {type(exercicio).__qualname__}")
-
-    # checagem de valor
-    if video < 0:
-        raise ValueError(f"o parâmetro 'video' não pode ser um número negativo")
-
-    # contador do exercício
-    try:
-        contador = __exercicios[exercicio]
-    except KeyError:
-        raise ValueError("exercício desconhecido: '{exercicio}'") from None
-
-    # contagem do exercício
-    try:
-        contador(video)
-    except Exception as error:
-        # formata o tipo de vídeo de entrada
-        if isinstance(video, str):
-            repr_video = "o arquivo '{video}'"
-        elif isinstance(video, int):
-            repr_video = "o dispositivo com índice {video}"
-
-        # gera uma exceção com 'error' como causa
-        raise RuntimeError("falha ao contar o exercício '{}' usando {repr_video} como entrada") from error
+# modulo interno com as funções já importadas,
+# seguro de remover a referência
+del _registro
 
