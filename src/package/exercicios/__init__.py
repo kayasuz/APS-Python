464389496869bc458265c6f62477737fee5fd140
Giovani Giaqueto de Oliveira <giovanigiaqueto@gmail.com>
2022-10-30 21:28:52 -0300

    Registro, listagem e contagem de exercícios
    adição de funções ao módulo package.exercícios para registrar e
    listar os exercícios com funções contagem implementadas, bem como
    uma função que conta a quantidade de exercícios em um vídeo sem
    conhecer o nome da função ou onde ela está localizada no código.
    
    registro do contador de flexões

--- src/package/exercicios/__init__.py
+++ src/package/exercicios/__init__.py
@@ -2,3 +2,69 @@
 from package.exercicios import flexoes
 
 __all__ = ["flexoes"]
+
+__exercicios = {}
+
+def registrar_exercicio(nome_exercicio):
+    """
+    registra a função seguinte como um contador de exercício
+    usando um decorador, o parâmetro deve ser o nome do exercício,
+    e a função retornada deve ser chamada no contador de exercício
+
+    exemplo:
+
+    @registrar_exercicio("polichinelo")
+    def contar_polichinelos(video):
+        ... implementação ...
+
+    """
+
+    if nome_exercicio in __exercicios:
+        raise RuntimeError(f"um exercício com o nome '{nome_exercicio}' já foi registrado")
+
+    # cria e retorna o decorador
+    def decorator(funcao):
+        global __exercicios
+        __exercicios[nome_exercicio] = funcao
+        return funcao
+
+    return decorador
+
+def listar_exercicios():
+    """
+    retorna uma lista de exercícios conhecidos
+    """
+    return list(__exercicios.keys())
+
+def contar_exercio(exercicio, video):
+    # checagem de tipos
+    if not isinstance(exercicio, str):
+        raise TypeError("esperado uma str para o parâmetro 'exercicio', "
+                        f"recebido um valor do tipo {type(exercicio).__qualname__}")
+    if not isinstance(video, (str, int)):
+        raise TypeError("esperado uma str ou int para o parâmetro 'video', "
+                        f"recebido um valor do tipo {type(exercicio).__qualname__}")
+
+    # checagem de valor
+    if video < 0:
+        raise ValueError(f"o parâmetro 'video' não pode ser um número negativo")
+
+    # contador do exercício
+    try:
+        contador = __exercicios[exercicio]
+    except KeyError:
+        raise ValueError("exercício desconhecido: '{exercicio}'") from None
+
+    # contagem do exercício
+    try:
+        contador(video)
+    except Exception as error:
+        # formata o tipo de vídeo de entrada
+        if isinstance(video, str):
+            repr_video = "o arquivo '{video}'"
+        elif isinstance(video, int):
+            repr_video = "o dispositivo com índice {video}"
+
+        # gera uma exceção com 'error' como causa
+        raise RuntimeError("falha ao contar o exercício '{}' usando {repr_video} como entrada") from error
+
