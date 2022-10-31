1d28d9cc8c7f3427958bd877fa987fef2ec61cfb
Giovani Giaqueto de Oliveira <giovanigiaqueto@gmail.com>
2022-10-30 21:46:09 -0300

    Atualização das dependências do pacote
    adição da biblioteca mediapipe nas dependências de instalação do pacote

--- setup.py
+++ setup.py
@@ -5,6 +5,7 @@ setup(
     name="package",
     version="0.0.1",
     python_requires=">=3.6",
+    install_requires=["mediapipe"],
     package_dir={"package": "src/package"},
     packages=["package", "package.exercicios", "package.exercicios.flexoes"]
 )
