fec6f98749463f4095013af2f502703336309acd
Giovani Giaqueto de Oliveira <giovanigiaqueto@gmail.com>
2022-11-03 11:38:20 -0300

    Melhorias no algorítimo contador de flexões
    - comparação da distância entre os pés e os ombros e entre os pés e os
      pulsos das mãos para verificar se as mãos estão na posição correta
    
    - verificação se as pernas estão retas e o corpo está na horizontal
    
    - adição da profundidade no cálculo das distâncias
    
    - distância entre os pulsos e os ombros usada na contagem levando
      em conta o tamanho da pessoa, diminuindo o impacto da distância
      e da resolução do vídeo na contagem

--- cntexercicios/exercicios/flexoes/__init__.py
+++ cntexercicios/exercicios/flexoes/__init__.py
@@ -2,6 +2,7 @@
 # bibliotecas de terceiros
 import cv2
 import mediapipe as mp
+import numpy as np
 # biblioteca padrão
 import os
 import sys
@@ -32,6 +33,10 @@ def contar_flexoes(video):
     # variável para controlar a contagem de flexões
     check = True
 
+    # lambda para simplificar a sintaxe
+    ponto_para_array = lambda ponto: np.array((ponto.x, 1-ponto.y, ponto.z))
+    posicao_landmark = lambda landmark: ponto_para_array(points.landmark[landmark])
+
     with abrir_video(video) as captura:
         for img in extrair_frames(captura):
 
@@ -45,21 +50,50 @@ def contar_flexoes(video):
 
             # variáveis para os pontos, com os pontos ajustados ao tamanho da tela
             if points:
-                ombroDx = int(points.landmark[pose.PoseLandmark.RIGHT_SHOULDER].x * w)
-                ombroDy = int(points.landmark[pose.PoseLandmark.RIGHT_SHOULDER].y * h)
-                pulsoDx = int(points.landmark[pose.PoseLandmark.RIGHT_WRIST].x * w)
-                pulsoDy = int(points.landmark[pose.PoseLandmark.RIGHT_WRIST].y * h)
+                # posições do centro dos pés, da cintura e dos ombros (base do pescoço)
+                pos_ombro_dir   = posicao_landmark(pose.PoseLandmark.RIGHT_SHOULDER)
+                pos_ombro_esq   = posicao_landmark(pose.PoseLandmark.LEFT_SHOULDER)
+                pos_cintura_dir = posicao_landmark(pose.PoseLandmark.RIGHT_HIP)
+                pos_cintura_esq = posicao_landmark(pose.PoseLandmark.LEFT_HIP)
+                pos_pe_dir      = posicao_landmark(pose.PoseLandmark.RIGHT_HEEL)
+                pos_pe_esq      = posicao_landmark(pose.PoseLandmark.LEFT_HEEL)
+                pos_pescoco     = ( pos_ombro_dir   + pos_ombro_esq   ) / 2
+                pos_cintura     = ( pos_cintura_dir + pos_cintura_esq ) / 2
+                pos_centro_pes  = ( pos_pe_dir      + pos_pe_esq      ) / 2
 
-                # distancia entre mão e o ombro
-                distMaoxombro = math.hypot(ombroDx - pulsoDx, ombroDy - pulsoDy)
+                # verificação se as pernas estão retas e o corpo está na horizontal
+                vet_pes_pescoco = pos_pescoco - pos_centro_pes
+                vet_pes_cintura = pos_cintura - pos_centro_pes
+                dis_pes_pescoco = np.linalg.norm(vet_pes_pescoco)
+                dis_pes_cintura = np.linalg.norm(vet_pes_cintura)
 
-                # contagem da flexão
-                if check == True and distMaoxombro <= 70:
-                    contador +=1
-                    check = False
+                # NOTE: o angulo é medido em relação ao eixo y dos frames
+                prod_vet = np.dot(vet_pes_pescoco, vet_pes_cintura) / (dis_pes_pescoco * dis_pes_cintura)
+                angulo   = np.degrees(np.fabs(np.arctan2(vet_pes_pescoco[0], vet_pes_pescoco[1])))
+                if prod_vet > 0.9 and 60 < angulo < 120:
+                    # verificação se a distância entre as mãos e os pés é próxima
+                    # da distância entre os pés e a base do pescoço
+                    pos_pulso_dir     = posicao_landmark(pose.PoseLandmark.RIGHT_WRIST)
+                    pos_pulso_esq     = posicao_landmark(pose.PoseLandmark.LEFT_WRIST)
+                    pos_centro_pulsos = ( pos_pulso_dir + pos_pulso_esq ) / 2
 
-                if distMaoxombro >=100 :
-                    check = True
+                    vet_pes_pulsos = pos_centro_pulsos - pos_centro_pes
+                    dis_pes_pulsos = np.linalg.norm(vet_pes_pulsos)
+
+                    if math.fabs(dis_pes_pulsos - dis_pes_pescoco) < (dis_pes_pulsos + dis_pes_pescoco) / 2:
+                        # aproximação da altura da flexão normalizada em relação a altura aproximada da pessoa
+                        # (distância entre os pés e a base do pescoço)
+                        h_flexao = (np.linalg.norm(pos_centro_pulsos - pos_pescoco) /
+                            np.linalg.norm(pos_pescoco - pos_centro_pes))
+
+                        # contagem da flexão
+                        if check:
+                            if h_flexao < 0.30:
+                                contador += 1
+                                check = False
+
+                        elif h_flexao >= 0.40:
+                            check = True
 
                 # texto na tela
                 texto = f'Contagem = {contador}'
