5954fd5579e98e0e653c9215afdce1b56312adbe
Giovani Giaqueto de Oliveira <giovanigiaqueto@gmail.com>
2022-11-03 13:28:58 -0300

    Atualização das dependências no setup.py
    adição da biblioteca numpy as dependências do pacote

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     name="contador-exercicios",
     version="0.0.1",
     python_requires=">=3.6",
-    install_requires=["mediapipe"],
+    install_requires=["mediapipe", "numpy"],
     package_dir={"cntexercicios": "cntexercicios"},
     packages=["cntexercicios", "cntexercicios.exercicios", "cntexercicios.exercicios.flexoes"]
 )
