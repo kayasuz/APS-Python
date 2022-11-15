4b9c4c19f282549934c77cb0df52a8eac909970b
Giovani Giaqueto de Oliveira <giovanigiaqueto@gmail.com>
2022-11-15 18:59:30 -0300

    Reorganização do arquivo setup.py para evitar conflitos de merge
    reorganização do setup.py para ajudar o git a resolver
    conflitos de merge, separando os módulos, dependências
    e outras informações em uma por linha.

--- setup.py
+++ setup.py
@@ -5,8 +5,18 @@ setup(
     name="contador-exercicios",
     version="0.2.0",
     python_requires=">=3.6",
-    install_requires=["mediapipe", "numpy"],
-    package_dir={"cntexercicios": "cntexercicios"},
-    packages=["cntexercicios", "cntexercicios.exercicios", "cntexercicios.exercicios.flexoes"]
+    install_requires=[
+        "mediapipe",
+        "numpy"
+    ],
+    package_dir={
+        "cntexercicios": "cntexercicios"
+    },
+    packages=[
+        "cntexercicios",
+        "cntexercicios.exercicios",
+        "cntexercicios.exercicios.flexoes",
+        "cntexercicios.exercicios.polichinelos"
+    ]
 )
 
